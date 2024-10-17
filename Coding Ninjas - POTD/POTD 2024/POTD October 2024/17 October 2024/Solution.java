8963536693f8bc3d78caf11a1c694984445fe659
Aniket A Khatavkar <aniketkhatawkar2001@gmail.com>
2024-10-17 18:27:53 +0530

    Add optimal Solution of Coding Ninjas POTD of 17 October 2024

---
+++ Coding Ninjas - POTD/POTD 2024/POTD October 2024/17 October 2024/Solution.java
@@ -0,0 +1,57 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+	public static int findKthElement(int[][] matrix, int k) {
+	    // Write your code here.
+		int n=matrix.length;
+		int m=matrix[0].length;
+
+		int top=0, bottom=n-1, left=0, right=m-1;
+		int count=0;
+
+		while(top<=bottom && left<=right){
+			for(int i=left; i<=right; i++){
+				count++;
+				if(count==k){
+					return matrix[top][i];
+				}
+			}
+
+			top++;
+
+			for(int i=top; i<=bottom; i++){
+				count++;
+				if(count==k){
+					return matrix[i][right];
+				}
+			}
+
+			right--;
+
+			if(top<=bottom){
+				for(int i=right; i>=left; i--){
+					count++;
+					if(count==k){
+						return matrix[bottom][i];
+					}
+				}
+
+				bottom--;
+			}
+
+			if(left<=right){
+				for(int i=bottom; i>=top; i--){
+					count++;
+					if(count==k){
+						return matrix[i][left];
+					}
+				}
+
+				left++;
+			}
+		}
+
+		return -1;
+	}
+}
