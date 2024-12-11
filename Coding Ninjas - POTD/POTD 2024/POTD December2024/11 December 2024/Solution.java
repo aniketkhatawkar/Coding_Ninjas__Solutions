821800cb920d07e9be689fc62574703fd7e8ef86
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-12-11 18:37:21 +0530

    Add optimal Solution of Coding Ninjas POTD of 11 December 2024

---
+++ Coding Ninjas - POTD/POTD 2024/POTD December2024/11 December 2024/Solution.java
@@ -0,0 +1,20 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+    public static int maximumPoints(int n, int[][] grid)
+    {
+       // Write your code here.
+       int ans=0;
+       
+       for(int i=0; i<n; i++){
+           for(int j=0; j<n; j++){
+               if(i==j || i+j==n-1) {
+                   ans+=grid[i][j];
+               }
+           }
+       }
+
+       return ans;
+    }
+}
