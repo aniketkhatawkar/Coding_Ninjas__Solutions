6beb1d9ed175d36961e18381a0529c39adc97bdc
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-12-24 19:18:39 +0530

    Add optimal Solution of Coding Ninjas POTD of 21, 22, 23 December 2024

---
+++ Coding Ninjas - POTD/POTD 2024/POTD December2024/24 December 2024/Solution.java
@@ -0,0 +1,25 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static boolean isPeriodic(String s) {
+        // Write your code here.
+        int n=s.length();
+
+        for(int i=1; i<=n/2; i++){
+            if(n%i==0){
+                String patt=s.substring(0, i);
+                StringBuilder sb = new StringBuilder();
+
+                for(int j=0; j<n/i; j++){
+                    sb.append(patt);
+                }
+
+                if(sb.toString().equals(s)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
