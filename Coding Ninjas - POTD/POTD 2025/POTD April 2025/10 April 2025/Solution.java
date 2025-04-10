b5944d92b1beae82f0264ac2c4a39b936a566851
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2025-04-10 16:33:50 +0530

    Add optimal Solution of Coding Ninjas POTD of 10 April  2025

---
+++ Coding Ninjas - POTD/POTD 2025/POTD April 2025/10 April 2025/Solution.java
@@ -0,0 +1,11 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+    static void printName(String s){
+        // Write your code here.
+        for(int i=0; i<5; i++){
+            System.out.println(s);
+        }
+    }
+}
